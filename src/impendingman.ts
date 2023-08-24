60d8fd829d058102be1c83dfb11958f09b3028ab
Brian Kessler <brian.kessler@enpal.de>
2023-08-24 16:46:36 +0200

    Added ability to save collection.json

--- src/impendingman.ts
+++ src/impendingman.ts
@@ -4,25 +4,29 @@ const shell = require('shelljs');
 import {ShellString} from "shelljs";
 import {transformCollection} from './transformTemplate';
 
+const customOutputFlag = 'impendingman-o';
+
 const args = minimist(process.argv.slice(2));
 const transformedContent = transformCollection(args);
-const {outputFilePath, transformedFilePath} = writeTransformedCollection();
+const {outputFilePath, transformedFilePath} = writeTransformedCollection(args);
 const newmanArgsAfterTemplate = extractNewmanArguments();
 const newmanCommand = `newman run ${transformedFilePath} ${newmanArgsAfterTemplate}`;
 const result = shell.exec(newmanCommand);
 cleanUp(outputFilePath, transformedFilePath);
 exit(result);
 
-function writeTransformedCollection() {
-    const outputFilePath = args.o;
+function writeTransformedCollection(args: (string | any)[]) {
+    // @ts-ignore
+    const outputFilePath = args[customOutputFlag];
     const transformedFilePath = outputFilePath ?? `./dist/transformed-collection.json`;
     shell.ShellString(transformedContent)
         .to(transformedFilePath);
     return {outputFilePath, transformedFilePath};
 }
+
 function extractNewmanArguments() {
     return process.argv.slice(3)
-        .filter(arg => arg !== '-o')
+        .filter(arg => arg !== customOutputFlag)
         .join(' ');
 }
 
@@ -34,9 +38,9 @@ function cleanUp(outputFilePath: string, transformedFilePath: string) {
 
 function exit(result: ShellString) {
     if (result.code !== 0) {
-        console.error('Newman execution failed');
+        console.error('Newman execution failed.');
     } else {
-        console.log('Newman executed successfully');
+        console.log('Newman executed successfully.');
     }
     process.exit(result.code);
 }
