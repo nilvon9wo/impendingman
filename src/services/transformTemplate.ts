4257f796419980f83f67314bee1b9a25e5403b29
Brian Kessler <brian.kessler@enpal.de>
2023-08-31 11:20:18 +0200

    Added support for non-stringifyed content

--- src/services/transformTemplate.ts
+++ src/services/transformTemplate.ts
@@ -1,11 +1,11 @@
 // noinspection ES6ConvertRequireIntoImport
-import {run} from "./runner";
-import {populate} from "./populator";
 
 const path = require('path');
 const shell = require('shelljs');
 
-const placeholderRegex = /\s*"{{{\s*file:\/\/\/\s*([^}]+)\s*}}}"\s*/g;
+//const placeholderRegex = /\s*"{{{\s*file:\/\/\/\s*([^}]+)\s*}}}"\s*/g;
+const placeholderRegex = /\s*(")?\s*{{{\s*file:\/\/\/\s*([^}"']+)\s*}}}\s*(")?\s*/g;
+
 const customMissingFileFlag = 'impending-man-on-missing-file';
 
 export function transformCollection(args: { _: any[]; }) {
@@ -24,18 +24,41 @@ function transformTemplate(
     const templateDir = path.dirname(templateFilePath);
     let match;
     while ((match = placeholderRegex.exec(templateContent)) !== null) {
-        const placeholder = match[0];
-        const filePath = match[1];
-            const absolutePath = path.join(templateDir, filePath)
-                .replace(/\\/g, '/');
-            const resolvedPath = path.resolve(absolutePath);
-            const scriptContent = shell.cat(resolvedPath)
-                .toString();
-            handleMissingFile(scriptContent, filePath, args);
-            templateContent = templateContent.replace(placeholder, ` ${JSON.stringify(scriptContent)}`);
+        templateContent = fillPlaceholder(match, templateDir, templateContent, args);
     }
 
     return templateContent;
+}
+
+function fillPlaceholder(
+        match: RegExpExecArray,
+        templateDir: string,
+        templateContent: string,
+        args: { _: any[] }
+    ) {
+    const placeholder = match[0];
+    const filePath = match[2];
+    const absolutePath = path.join(templateDir, filePath)
+        .replace(/\\/g, '/');
+    const resolvedPath = path.resolve(absolutePath);
+    const scriptContent = shell.cat(resolvedPath)
+        .toString();
+    handleMissingFile(scriptContent, filePath, args);
+    const resolvedContent = resolve(match, scriptContent);
+    return templateContent.replace(placeholder, ` ${JSON.stringify(resolvedContent)}`);
+}
+
+function resolve(match: RegExpExecArray, scriptContent: string) {
+    const hasOpeningQuote = match[1] !== undefined;
+    const hasClosingQuote = match[3] !== undefined;
+    const isQuoted = hasOpeningQuote && hasClosingQuote;
+    if (!isQuoted && (hasOpeningQuote || hasClosingQuote)) {
+        throw new Error(`Imbalanced quotes around placeholder ${match[0]}`);
+    }
+
+    return isQuoted
+        ? JSON.stringify(scriptContent)
+        : scriptContent;
 }
 
 function handleMissingFile(scriptContent: any, filePath: string, args: { _: any[]; }) {
