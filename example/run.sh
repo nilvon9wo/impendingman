e5db984a70b6ff43049335a817f0f81f2c47c4b5
Brian Kessler <brian.kessler@enpal.de>
2023-09-01 17:50:33 +0200

    Demonstrating error codes

--- example/run.sh
+++ example/run.sh
@@ -4,4 +4,9 @@ node ../dist/impendingman.js run "./example.postman_collection.template.json" \
   --env-var TARGET_DOMAIN=catfact.ninja \
   --impending-man-output "./catfacts.postman_collection.json"
 
-
+# Check if all runs were successful
+if [ $? -eq 0 ]; then
+  echo "Success: Tests passed! :-)"
+else
+  echo "Failure: Tested failed. :-("
+fi
--- example/run_from_npm.sh
+++ example/run_from_npm.sh
@@ -4,4 +4,9 @@ impendingman run "./example.postman_collection.template.json" \
   --env-var TARGET_DOMAIN=catfact.ninja \
   --impending-man-output "./catfacts.postman_collection.json"
 
-
+# Check if all runs were successful
+if [ $? -eq 0 ]; then
+  echo "Success: Tests passed! :-)"
+else
+  echo "Failure: Tested failed. :-("
+fi
