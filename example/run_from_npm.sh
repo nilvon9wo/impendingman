acd432dbf1ede7c1abe0cd5efa178d6a24611cda
Brian Kessler <brian.kessler@enpal.de>
2023-09-01 17:37:49 +0200

    Changing dev dependencies to dependencies

---
+++ example/run_from_npm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+impendingman run "./example.postman_collection.template.json" \
+  --env-var TARGET_DOMAIN=catfact.ninja \
+  --impending-man-output "./catfacts.postman_collection.json"
+
+
