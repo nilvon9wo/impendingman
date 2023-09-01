b332597e2277ac5be6f9c01cbf88e222bb17b5d9
Brian Kessler <brian.kessler@enpal.de>
2023-09-01 17:14:32 +0200

    Trying to fix npm package

--- example/run.sh
+++ example/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node ../dist/impendingman.js run "./example.postman_collection.template.json" \
+node ../dist/impendingman run "./example.postman_collection.template.json" \
   --env-var TARGET_DOMAIN=catfact.ninja \
   --impending-man-output "./catfacts.postman_collection.json"
 
